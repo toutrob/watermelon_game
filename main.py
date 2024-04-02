55e507ad6af0af7350b77bb7b5016a2816b66e94
Arthur Mathieu <0arthur0mathieu0@gmail.com>
2024-04-02 16:23:34 +0200

    Ajout de la fenêtre d'affichage graphique

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
-# This is a sample Python script.
+import pygame
 
-# Press Maj+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+pygame.init()
 
+window_size = (1200, 700)
+window = pygame.display.set_mode(window_size)
+pygame.display.set_caption("My Pygame Window")
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+running = True
+while running:
+    # Handle events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
 
+    # Draw shapes
+    ##window.fill((255, 255, 255))
+    ##pygame.draw.circle(window, (255, 255, 0), (150, 200), 50)
+    ##pygame.draw.rect(window, (0, 200, 0), (100, 300, 300, 200))
+    ##pygame.draw.line(window, (0, 0, 100), (100, 100), (700, 500), 5)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    pygame.display.flip()
+pygame.quit()
