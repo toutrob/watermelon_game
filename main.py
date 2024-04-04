c1e842a86d99a8e3544c0fe5a30f0c3eda8d61b3
robin-r <robinrus42@gmail.com>
2024-04-04 17:30:24 +0200

    ajout des boules sur le clic de la souris

--- main.py
+++ main.py
@@ -15,10 +15,18 @@ space = pymunk.Space()
 space.gravity = (0, -500)
 
 ball_body = pymunk.Body(1, 100)
-ball_body.position = (500, 500)
+ball_body.position = (500, 700)
 
 ball_shape = pymunk.Circle(ball_body, 20)
 space.add(ball_body, ball_shape)
+
+
+def create_ball(position):
+    ball_body = pymunk.Body(1, 100)
+    ball_body.position = position
+    ball_shape = pymunk.Circle(ball_body, 20)
+    space.add(ball_body, ball_shape)
+
 
 shape1 = pymunk.Segment(
     space.static_body, (400, 600), (400, 100), 0.0
@@ -44,17 +52,30 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+            mouse_position = pygame.mouse.get_pos()
+            create_ball(mouse_position)
+
 
     space.step(1/60)
-    ball_pos = int(ball_body.position.x), window_size[1] - int(ball_body.position.y)
-    pygame.draw.circle(window, BLUE, ball_pos, 20)
-    pygame.display.flip()
-    clock.tick(60)
 
     window.fill((249, 228, 183))
+
+    for body in space.bodies:
+        for shape in body.shapes:
+            if isinstance(shape, pymunk.Circle):
+                pos_x, pos_y = int(body.position.x), window_size[1] - int(body.position.y)
+                pygame.draw.circle(window, BLUE, (pos_x, pos_y), int(shape.radius))
+
+
     pygame.draw.line(window, (88, 41, 0), (400, 100), (400, 600), 7)
     pygame.draw.line(window, (88, 41, 0), (800, 100), (800, 600), 7)
     pygame.draw.line(window, (88, 41, 0), (400, 600), (800, 600), 7)
+
+    pygame.display.flip()
+
+    clock.tick(60)
+
 
     # Draw shapes
     ##window.fill((255, 255, 255))
@@ -62,5 +83,5 @@ while running:
     ##pygame.draw.rect(window, (0, 200, 0), (100, 300, 300, 200))
     ##pygame.draw.line(window, (0, 0, 100), (100, 100), (700, 500), 5)
 
-    pygame.display.flip()
+
 pygame.quit()
