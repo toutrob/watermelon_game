cbca83578ecab2bf20a9eb9013cd925252ac07c1
robin-r <robinrus42@gmail.com>
2024-05-07 13:52:43 +0200

    supression d'anciens codes commentés

--- main.py
+++ main.py
@@ -9,77 +9,6 @@ WINDOWSIZE = (1200, 700)
 window = pygame.display.set_mode(WINDOWSIZE)
 pygame.display.set_caption("Watermelon Game")
 
-'''class boules :
-    def __init__(self,fenetre, centre, rayon, masse, type, sprite):
-        self.fenetre = fenetre
-        self.rayon = rayon
-        self.masse = masse
-        self.type = type
-        self.sprite = sprite
-        self.centre = centre
-        self.centremasque = pymunk.Body(1, 100)
-
-    def gravite(self):
-        ''''''centremasque = pymunk.Body(self.masse, 100)'''''''
-        self.centremasque.position = self.centre
-
-        ball_shape = pymunk.Circle(self.centremasque, self.rayon)
-        ball_shape.ball = self
-        ball_shape.collision_type = self.type
-        space.add(self.centremasque, ball_shape)
-
-
-
-    def dessin(self):
-        ball_pos = int(self.centremasque.position.x), window_size[1] - int(self.centremasque.position.y)
-        pygame.draw.circle(self.fenetre, self.sprite, ball_pos, self.rayon)
-
-    def trouver_par_position(cls, x, y):
-        for instance in cls.instances:
-            if instance.x == x and instance.y == y:
-                return instance
-        return None
-
-    def dessin_preview(self):
-        pygame.draw.circle(self.fenetre, self.sprite, self.centre, self.rayon)
-
-
-class boule1(boules):
-    def __init__(self, centre):
-        super().__init__(window, centre, 10, 100, 5 , (0,255,0) )
-
-class boule2(boules):
-    def __init__(self, centre):
-        super().__init__(window, centre, 20, 200, 6 , (255,0,0) )
-
-class boule3(boules):
-    def __init__(self, centre):
-        super().__init__(window, centre, 35, 350, 7, (0,0,255))
-
-class boule4(boules):
-    def __init__(self, centre):
-        super().__init__(window, centre, 45, 400, 8, (255,255,0))
-
-class boule5(boules):
-    def __init__(self, centre):
-        super().__init__(window, centre, 55, 450, 9, (0,255,255))
-
-class boule6(boules):
-    def __init__(self, centre):
-        super().__init__(window, centre, 65, 500, 10, (255,0,255))
-
-class boule7(boules):
-    def __init__(self, centre):
-        super().__init__(window, centre, 75, 550, 11, (130,255,130))
-
-class boule8(boules):
-    def __init__(self, centre):
-        super().__init__(window, centre, 85, 600, 12, (130,130,255))'''
-
-
-
-
-
 
 pygame.init()
 
@@ -323,7 +252,7 @@ while running:
                         bottom_y = shape.body.position.y - shape.radius  # Position en y du bas de la boule
                         if bottom_y > 550:  # Si la boule dépasse une certaine hauteur (par exemple 600 pixels)
                             if body in time_elapsed:
-                                if time.time() - time_elapsed[body] > 5:
+                                if time.time() - time_elapsed[body] > 3:
                                     game_over = True
 
                             else:
